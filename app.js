7c7bb37b72d870629e415892fb5c17843c19a3d5
gaurav101203 <162018942+gaurav101203@users.noreply.github.com>
2025-01-01 14:30:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -95,7 +95,9 @@ async function fetchTrends() {
         .addArguments("--no-sandbox")
         .addArguments('--window-size=1920,1080');
         // .addArguments(`--proxy-server=${PROXY}`);
-    
+
+        const service = new chrome.ServiceBuilder(driverPath).build(); // Correct instantiation
+        service.start();  // Start the Chrome service
         const driver = await new Builder().forBrowser("chrome").setChromeOptions(options)
         .setChromeService(service)
         .build();
