6a7f8dcfbbac594f0e58e4cb30453d19d0dcc2f3
gaurav101203 <162018942+gaurav101203@users.noreply.github.com>
2025-01-01 18:48:17 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,20 +3,20 @@
 apt-get update
 apt-get install -y wget unzip ca-certificates fonts-liberation libappindicator3-1 libasound2 libatk-bridge2.0-0 libatk1.0-0 libcups2 libdrm2 libgbm1 libnspr4 libnss3 libx11-xcb1 libxcomposite1 libxdamage1 libxrandr2 xdg-utils
 
-# Install Chrome
+# Install Google Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb || apt-get -f install -y
+apt-get update
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
 
-# Move Chrome to the correct directory
-ln -s /usr/bin/google-chrome /usr/bin/chrome
 
 # Install ChromeDriver
-CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+\.\d+')
-wget https://chromedriver.storage.googleapis.com/$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE)/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-chmod +x chromedriver
-mv chromedriver /usr/local/bin/chromedriver
-ln -s /usr/local/bin/chromedriver /usr/bin/chromedriver
+CHROME_VERSION=$(google-chrome --version | grep -oP '\d+' | head -1)
+wget https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip -d /usr/local/bin
+chmod +x /usr/local/bin/chromedriver
+rm chromedriver_linux64.zip
+
 
 # Verify installation
 if [ -f /usr/bin/google-chrome ]; then
