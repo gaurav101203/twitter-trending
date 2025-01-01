09520098766e8ea01f15d6b2aa2f6409622bc937
gaurav101203 <162018942+gaurav101203@users.noreply.github.com>
2025-01-01 14:06:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -85,6 +85,8 @@ async function storeTrendingTopics(trends, ipAddress) {
     }
 }
 
+const service = new chrome.ServiceBuilder(driverPath).build(); // Correct instantiation
+        service.start();  // Start the Chrome service
 // Main function
 async function fetchTrends() {
     const options = new chrome.Options()
@@ -93,9 +95,7 @@ async function fetchTrends() {
         .addArguments("--no-sandbox")
         .addArguments('--window-size=1920,1080');
         // .addArguments(`--proxy-server=${PROXY}`);
-
-        const service = new chrome.ServiceBuilder(driverPath).build(); // Correct instantiation
-        service.start();  // Start the Chrome service
+    
         const driver = await new Builder().forBrowser("chrome").setChromeOptions(options)
         .setChromeService(service)
         .build();
