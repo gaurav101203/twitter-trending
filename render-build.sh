2b943b8ae983445e4681ed1226489d0f23e26fec
gaurav101203 <162018942+gaurav101203@users.noreply.github.com>
2025-01-01 13:30:57 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
-# Install Chrome
-curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
 
-# Install ChromeDriver
-CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+\.\d+' | head -n 1)
-CHROME_DRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
-curl -O "https://chromedriver.storage.googleapis.com/${CHROME_DRIVER_VERSION}/chromedriver_linux64.zip"
-unzip chromedriver_linux64.zip -d /usr/local/bin/
+# Ensure binaries are executable
+chmod +x ./chromium
+chmod +x ./chromedriver
+
+# Install npm dependencies
+npm install
